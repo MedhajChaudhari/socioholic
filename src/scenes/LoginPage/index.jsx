37fcedbe53afecba09d99bbf630113a1d985bd03
MedhajChaudhari <medhaj.c@somaiya.edu>
2024-08-19 22:04:18 +0530

    https://github.com/MedhajChaudhari/sociaoholic

---
+++ src/scenes/LoginPage/index.jsx
@@ -0,0 +1,21 @@
+
+const LoginPage = () => {
+    return <div>Login Page</div>
+};
+
+export default LoginPage;
+
+
+/*
+import LoginPage from "scenes/LoginPage"; // Adjust the path as necessary
+
+const App = () => {
+    return (
+        <div>
+            LoginPage 
+        </div>
+    );
+};
+
+export default App;
+*/
